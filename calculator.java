3ddb4f804cc67bdfbe44cc7e16f64ba4002976f6
krishnaa8954 <ky60678@gmail.com>
2025-08-30 20:18:05 +0530

    [krishna] add : calculator with add and sub features

---
+++ calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class calculator{
+    public static int add( int a, int b) {
+        return a+b;
+    }
+
+    public static int sub( int a, int b) {
+        return a-b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int num1 =  sc.nextInt();
+         int num2 = sc.nextInt();
+        int add = add(num1, num2);
+        int sub =  sub(num1, num2);
+        
+        System.out.println(add);
+        System.out.println(sub);
+
+    }
+}
